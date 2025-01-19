5d11b599e7698a6b3c6aa7bb4703ef7392e87421
Matt Bannert <bunny@hey.com>
2025-01-19 15:43:35 +0100

    basic setup to spin up hetzner server using terraform

---
+++ main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.46.1"
+    }
+  }
+}
+
+provider "hcloud" {
+    token = var.hetzner_cloud_api_token
+}
+
+resource "hcloud_firewall" "web_server_and_ssh" {
+    name = "Standard Web Ports and SSH"
+    rule {
+        description = "Allow HTTP traffic"
+        direction   = "in"
+        protocol    = "tcp"
+        port        = "80"
+        source_ips = [
+        "0.0.0.0/0",
+        "::/0"
+        ]  
+  }
+
+    rule {
+    description = "Allow HTTPS traffic"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+    rule {
+    description = "Allow HTTPS traffic"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "8080"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+    rule {
+    description = "Allow SSH traffic"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "22"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
+
+resource "hcloud_network" "hood" {
+    name = "The hood"
+    ip_range = "10.0.1.0/16"
+
+}
+
+resource "hcloud_network_subnet" "corner_se" {
+    network_id = hcloud_network.hood.id
+    type = "cloud"
+    ip_range = "10.0.1.0/24"
+    network_zone = "eu-central"
+}
+
+data "hcloud_ssh_key" "stash" {
+    name = "stash"
+}
+
+# https://docs.hetzner.com/cloud/general/locations/
+resource "hcloud_server" "stage" {
+    name = "stage"
+    server_type = "cpx11"
+    location = "nbg1"
+    image = "docker-ce"
+    ssh_keys = [data.hcloud_ssh_key.stash.id]
+    firewall_ids = [ hcloud_firewall.web_server_and_ssh.id ]
+
+    network {
+      network_id = hcloud_network.hood.id
+    }
+
+    depends_on = [hcloud_network_subnet.corner_se]
+ 
+}
